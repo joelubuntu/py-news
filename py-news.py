6311e3f43d5477b7134ce46420f208254486dc72
joelubuntu <72544077+joelubuntu@users.noreply.github.com>
2023-01-15 17:02:32 +0530

    Update py-news.py

--- py-news.py
+++ py-news.py
@@ -8,11 +8,11 @@ req_ip = urllib.request.urlopen('http://ip-api.com/json/' + user_json_value['ip'
 req_ip_json = json.loads(req_ip.read())
 country_code = req_ip_json['countryCode'].lower()
 
-newsapi = NewsApiClient(api_key='7458b6f09d3c4e77b1c0b95beeb57ad9')
-top_headlines = newsapi.get_top_headlines(language='en',country="in")
-
-for i in range(len(top_headlines['articles'])):
-	print('\n'+ '•' + "Title: " + top_headlines['articles'][i]['title'])
-	print("Description: " + top_headlines['articles'][i]['description'])
-	print("Content: " + top_headlines['articles'][i]['content']+"\n")
-	print("Read me: " + top_headlines['articles'][i]['url'])
+if len(sys.argv) == 1:
+	newsapi = NewsApiClient(api_key='7458b6f09d3c4e77b1c0b95beeb57ad9')
+	top_headlines = newsapi.get_top_headlines(language='en',country="in")
+	for i in range(len(top_headlines['articles'])):
+		print('\n'+ '•' + "Title: " + str(top_headlines['articles'][i]['title']))
+		print("Description: " + str(top_headlines['articles'][i]['description']))
+		print("Content: " + str(top_headlines['articles'][i]['content']))
+		print("Read more: " + str(top_headlines['articles'][i]['url']) + "\n")
